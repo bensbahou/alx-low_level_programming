12996fe5d74154cf4d6200ec69325d05f4719912
bensbahou <younesbensbahou@gmail.com>
2023-03-23 03:26:16 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the numbers, from 0 to 14
+ * more_numbers - print 10 times the numbers
+ * from 0 to 14
  * inputs : void
  * Return: void
  */
