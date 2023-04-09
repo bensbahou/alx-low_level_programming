0d715b62f63ccd441894c71326af5522bbdc7e39
bensbahou <younesbensbahou@gmail.com>
2023-04-08 17:09:47 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,21 +6,31 @@
  * @argc: argument count
  * @argv: arguments
  *
- * Return: 0 if no error, 1 elsewhere
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
 	int i, n, sum = 0;
+	char *flag;
 
-	for (i = 1; i < argc; i++)
+	if (argc < 2)
 	{
-		n = atoi(argv[i]);
-		if (n == 0 && argv[i] != "0")
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*flag)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += n;
+		else
+		{
+			sum += n;
+		}
 	}
 	printf("%d\n", sum);
 
