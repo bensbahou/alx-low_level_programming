a6fe4b2b10b2b76ebf2ade8b1855964b5b39fdb6
bensbahou <younesbensbahou@gmail.com>
2023-04-16 20:39:49 -0700

    define dog struct in dog.h

---
+++ 0x0E-structures_typedef/REAMDE.md
@@ -0,0 +1,1 @@
+REAMDE for 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - represent dog object
+ *
+ * @name: Name
+ *
+ * @age: Age
+ *
+ * @owner: Owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
