8cab65b3d6aa793a1c5433bf9e99d0d2afaec05a
bensbahou <younesbensbahou@gmail.com>
2023-04-19 14:08:01 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - the main function
+  * @argc: arguments count passed
+  * @argv: list of arguments
+  *
+  * Return: 0 if everything is ok
+  */
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	oprt = get_op_func(argv[2]);
+
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
