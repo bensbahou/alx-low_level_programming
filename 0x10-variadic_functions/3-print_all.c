3c678304822059f871d7cd85c86b737414f6cc0a
bensbahou <younesbensbahou@gmail.com>
2023-04-20 08:13:54 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,24 +22,20 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(a_list, int));
-				if (format[i + 1] != '\0')
-					printf(", ");
+				(format[i + 1] != '\0') ? printf(", "): printf("%s","");
 				break;
 			case 'i':
 				printf("%i", va_arg(a_list, int));
-				if (format[i + 1] != '\0')
-					printf(", ");
+				(format[i + 1] != '\0') ? printf(", "): printf("%s","");
 				break;
 			case 'f':
 				printf("%f", va_arg(a_list, double));
-				if (format[i + 1] != '\0')
-					printf(", ");
+				(format[i + 1] != '\0') ? printf(", "): printf("%s","");
 				break;
 			case 's':
 				str = va_arg(a_list, char*);
 				printf("%s", str ? str : "(nil)");
-				if (format[i + 1] != '\0')
-					printf(", ");
+				(format[i + 1] != '\0') ? printf(", "): printf("%s","");
 				break;
 			default:
 				break;
