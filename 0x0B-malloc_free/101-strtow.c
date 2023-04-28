aa8cbf915e72b570c51a93a6021ea03de977b21a
bensbahou <younesbensbahou@gmail.com>
2023-04-28 06:41:16 -0700

    101-strtow.c empty function

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * strtow - splits a string into words.
+ * @str: the string to split
+ *
+ * Return: returns a pointer to an array of strings
+ */
+
+char **strtow(char *str)
+{
+	char **words;
+
+	return (words);
+}
