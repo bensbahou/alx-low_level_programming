6b7ba593bbe625ec352d63c3cd8cbf49706ab618
bensbahou <younesbensbahou@gmail.com>
2023-05-08 04:25:18 -0700

    empty 100-elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+/**
+ * main - Displays the information contained in the
+ * ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[1] == 0 || argc != 2)
+		exit(98);
+	return (0);
+}
