32e0922dd3f470fcf47cb3c7f3c18c10968607f4
bensbahou <younesbensbahou@gmail.com>
2023-05-08 04:38:52 -0700

    add read

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -10,7 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int file_descriptor;
+	int file_descriptor, bytes_read;
+	FILE *header;
 
 	file_descriptor = open(argv[1], O_RDONLY);
 	if (file_descriptor == -1)
@@ -18,8 +19,16 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	bytes_read = read(file_descriptor, header, 1024);
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
 	if (argc != 2)
 		exit(98);
+
 	close(file_descriptor);
 	return (0);
 }
