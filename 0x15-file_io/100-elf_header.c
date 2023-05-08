7cfc301f4cc63348a9dfb1b0f3c6aa2fdc1c6e84
bensbahou <younesbensbahou@gmail.com>
2023-05-08 04:29:10 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - Displays the information contained in the
  * ELF header at the start of an ELF file.
@@ -8,7 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	if (argv[1] == 0 || argc != 2)
+	int file_descriptor;
+	file_descriptor = open(argv[1], O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (argc != 2)
 		exit(98);
 	return (0);
 }
