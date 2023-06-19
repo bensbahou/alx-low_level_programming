90356ab725f9e21bb71aac94b5a399e323df4f7c
bensbahou <younesbensbahou@gmail.com>
2023-06-19 12:03:45 -0700

    my commit

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,36 @@
+int add(int i, int k);
+int sub(int i, int k);
+int mul(int i, int k);
+int div(int i, int k);
+int mod(int i, int k);
+
+
+int add(int i, int k)
+{
+	return (i + k);
+}
+
+int sub(int i, int k)
+{
+	return (i - k);
+}
+ 
+
+int mul(int i, int k)
+{
+	return (i * k);
+}
+
+ 
+
+int div(int i, int k)
+{
+	return (i / k);
+}
+
+ 
+
+int mod(int i, int k)
+{
+	return (i % k);
+}
