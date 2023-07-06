dad7fdf214b58e3e8318a0d6f9accbc1579e35b7
bensbahou <younesbensbahou@gmail.com>
2023-07-06 12:19:39 -0700

    my commit

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,8 @@
  * @ht: The hash table to look into
  * @key: key to look for
  *
- * Return: value associated with the element, or NULL if key couldn’t be found
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
