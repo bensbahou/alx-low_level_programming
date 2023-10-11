a41f01f84d62ad715507f05a263b04612d54aeb1
bensbahou <younesbensbahou@gmail.com>
2023-10-11 11:49:06 +0100

    1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_search - Searches for a value in a sorted array of integers
- * 			   using the Binary search algorithm.
+ * using the Binary search algorithm.
  * @array: pointer to the first element of the array to search in.
  * @size: number of elements in array.
  * @value: value to search for.
